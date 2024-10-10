9717535b980fc889f20a03d5b52d35e5b7b59e87
Abed <abedkansof18@gmail.com>
2024-10-10 20:01:37 +0300

    Some Notes and fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Passenger;  // Import the Passenger model
 use App\Models\Flight;  // Import the Passenger model
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,12 +17,29 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        // Create 50 flights using the factory
-      //  Flight::factory()->count(50)->create();       
-        Flight::factory()->count(5)->create();    
+      // Create 50 flights using the factory
+      // Flight::factory()->count(50)->create();       
+       Flight::factory()->count(10)->create();    
 
-        // Create 1000 passengers using the factory
+      // Create 1000 passengers using the factory
       // Passenger::factory()->count(1000)->create();  
-        Passenger::factory()->count(2)->create();
+       Passenger::factory()->count(5)->create();
+
+  
+      // large data samples can be seeded but it take time 
+      // and errors in relational data occured
+
+      // User Samples for Excel import
+      User::factory()->count(10)->create();
+
+      //Note: An Admin role was added in the MySQL Databse
+      // for API testing for roles using the following SQL statements:
+
+      // INSERT INTO roles (name, created_at, updated_at) VALUES 
+      // ('admin', NOW(), NOW());  
+
+      // INSERT INTO role_user (user_id, role_id, created_at, updated_at) 
+      // VALUES (1, 1, NOW(), NOW());
+      
     }
 }
